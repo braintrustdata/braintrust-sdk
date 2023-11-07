0705a6b0420e51294f1de618d33fee32b232ba08
Ankur Goyal <ankrgyl@gmail.com>
2023-11-07 00:10:01 -0800

    Add support for python v1

--- py/src/braintrust/oai.py
+++ py/src/braintrust/oai.py
@@ -3,9 +3,18 @@ import time
 from .logger import current_span
 
 
+class NamedWrapper:
+    def __init__(self, wrapped):
+        self.__wrapped = wrapped
+
+    def __getattr__(self, name):
+        return getattr(self.__wrapped, name)
+
+
 class ChatCompletionWrapper:
-    def __init__(self, chat):
-        self.chat = chat
+    def __init__(self, create_fn, acreate_fn):
+        self.create_fn = create_fn
+        self.acreate_fn = acreate_fn
 
     def create(self, *args, **kwargs):
         params = self._parse_params(kwargs)
@@ -15,14 +24,14 @@ class ChatCompletionWrapper:
         should_end = True
         try:
             start = time.time()
-            response = self.chat.create(*args, **kwargs)
+            raw_response = self.create_fn(*args, **kwargs)
             if stream:
 
                 def gen():
                     try:
                         first = True
                         all_results = []
-                        for item in response:
+                        for item in raw_response:
                             if first:
                                 span.log(
                                     metrics={
@@ -30,24 +39,25 @@ class ChatCompletionWrapper:
                                     }
                                 )
                                 first = False
-                            all_results.append(item)
+                            all_results.append(item if isinstance(item, dict) else item.dict())
                             yield item
                         span.log(output=all_results)
                     finally:
                         span.end()
 
                 should_end = False
-                return (x for x in gen())
+                return gen()
             else:
+                log_response = raw_response if isinstance(raw_response, dict) else raw_response.dict()
                 span.log(
                     metrics={
-                        "tokens": response["usage"]["total_tokens"],
-                        "prompt_tokens": response["usage"]["prompt_tokens"],
-                        "completion_tokens": response["usage"]["completion_tokens"],
+                        "tokens": log_response["usage"]["total_tokens"],
+                        "prompt_tokens": log_response["usage"]["prompt_tokens"],
+                        "completion_tokens": log_response["usage"]["completion_tokens"],
                     },
-                    output=response["choices"][0],
+                    output=log_response["choices"][0],
                 )
-                return response
+                return raw_response
         finally:
             if should_end:
                 span.end()
@@ -60,14 +70,14 @@ class ChatCompletionWrapper:
         should_end = True
         try:
             start = time.time()
-            response = await self.chat.acreate(*args, **kwargs)
+            raw_response = await self.acreate_fn(*args, **kwargs)
             if stream:
 
                 async def gen():
                     try:
                         first = True
                         all_results = []
-                        async for item in response:
+                        async for item in raw_response:
                             if first:
                                 span.log(
                                     metrics={
@@ -75,24 +85,25 @@ class ChatCompletionWrapper:
                                     }
                                 )
                                 first = False
-                            all_results.append(item)
+                            all_results.append(item if isinstance(item, dict) else item.dict())
                             yield item
                         span.log(output=all_results)
                     finally:
                         span.end()
 
                 should_end = False
-                return (x async for x in gen())
+                return gen()
             else:
+                log_response = raw_response if isinstance(raw_response, dict) else raw_response.dict()
                 span.log(
                     metrics={
-                        "tokens": response["usage"]["total_tokens"],
-                        "prompt_tokens": response["usage"]["prompt_tokens"],
-                        "completion_tokens": response["usage"]["completion_tokens"],
+                        "tokens": log_response["usage"]["total_tokens"],
+                        "prompt_tokens": log_response["usage"]["prompt_tokens"],
+                        "completion_tokens": log_response["usage"]["completion_tokens"],
                     },
-                    output=response["choices"][0],
+                    output=log_response["choices"][0],
                 )
-                return response
+                return raw_response
         finally:
             if should_end:
                 span.end()
@@ -106,17 +117,61 @@ class ChatCompletionWrapper:
             "metadata": params,
         }
 
-    def __getattr__(self, name):
-        return getattr(self.chat, name)
+
+class ChatCompletionV0Wrapper(NamedWrapper):
+    def __init__(self, chat):
+        self.__chat = chat
+        super().__init__(chat)
+
+    def create(self, *args, **kwargs):
+        return ChatCompletionWrapper(self.__chat.create, self.__chat.acreate).create(*args, **kwargs)
+
+    async def acreate(self, *args, **kwargs):
+        return await ChatCompletionWrapper(self.__chat.create, self.__chat.acreate).acreate(*args, **kwargs)
 
 
-class OpenAIWrapper:
+# This wraps 0.*.* versions of the openai module, eg https://github.com/openai/openai-python/tree/v0.28.1
+class OpenAIV0Wrapper(NamedWrapper):
     def __init__(self, openai):
-        self.openai = openai
-        self.ChatCompletion = ChatCompletionWrapper(openai.ChatCompletion)
+        super().__init__(openai)
+        self.ChatCompletion = ChatCompletionV0Wrapper(openai.ChatCompletion)
 
-    def __getattr__(self, name):
-        return getattr(self.openai, name)
+
+class CompletionsV1Wrapper(NamedWrapper):
+    def __init__(self, completions):
+        self.__completions = completions
+        super().__init__(completions)
+
+    def create(self, *args, **kwargs):
+        return ChatCompletionWrapper(self.__completions.create, None).create(*args, **kwargs)
+
+
+class AsyncCompletionsV1Wrapper(NamedWrapper):
+    def __init__(self, completions):
+        self.__completions = completions
+        super().__init__(completions)
+
+    async def create(self, *args, **kwargs):
+        return await ChatCompletionWrapper(None, self.__completions.create).acreate(*args, **kwargs)
+
+
+class ChatV1Wrapper(NamedWrapper):
+    def __init__(self, chat):
+        super().__init__(chat)
+
+        import openai
+
+        if type(chat.completions) == openai.resources.chat.completions.AsyncCompletions:
+            self.completions = AsyncCompletionsV1Wrapper(chat.completions)
+        else:
+            self.completions = CompletionsV1Wrapper(chat.completions)
+
+
+# This wraps 1.*.* versions of the openai module, eg https://github.com/openai/openai-python/tree/v1.1.0
+class OpenAIV1Wrapper(NamedWrapper):
+    def __init__(self, openai):
+        super().__init__(openai)
+        self.chat = ChatV1Wrapper(openai.chat)
 
 
 def wrap_openai(openai):
@@ -125,4 +180,7 @@ def wrap_openai(openai):
 
     :param openai: The openai module
     """
-    return OpenAIWrapper(openai)
+    if hasattr(openai, "chat") and hasattr(openai.chat, "completions"):
+        return OpenAIV1Wrapper(openai)
+    else:
+        return OpenAIV0Wrapper(openai)
