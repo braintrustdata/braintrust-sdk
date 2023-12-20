000ab71d3bb76f39bea9b8b9149e3c7c8cc9c123
Manu Goyal <manu.goyal2013@gmail.com>
2023-12-20 11:36:29 -0800

    Undo oai-related changes.

--- js/src/stackutil.ts
+++ js/src/stackutil.ts
@@ -33,27 +33,8 @@ export function getStackTrace(): StackTraceEntry[] {
   return out;
 }
 
-// Emulates the Path.parents approach in sdk/py/src/braintrust/util.py by
-// iterating over the parents of `path`, and checking if `directory`
-// matches any of them.
-function isSubpath(testPath: string, directory: string) {
-  testPath = path.normalize(testPath);
-  directory = path.normalize(directory);
-  if (testPath === directory) {
-    return true;
-  }
-  const maxIters = testPath.split(path.sep).length + 1;
-  for (let i = 0; i < maxIters; ++i) {
-    const parentDir = path.dirname(testPath);
-    if (parentDir === directory) {
-      return true;
-    }
-  }
-  return false;
-}
-
 // Fetches the first StackTraceEntry not contained inside the same directory as
-// this file (or any of its subdirectories) and converts it into a CallerLocation.
+// this file and converts it into a CallerLocation.
 export function getCallerLocation(): CallerLocation | undefined {
   let thisDir: string | undefined = undefined;
   const entries = getStackTrace();
@@ -61,7 +42,7 @@ export function getCallerLocation(): CallerLocation | undefined {
     if (thisDir === undefined) {
       thisDir = path.dirname(frame.fileName);
     }
-    if (!isSubpath(frame.fileName, thisDir)) {
+    if (path.dirname(frame.fileName) !== thisDir) {
       return {
         caller_functionname: frame.functionName,
         caller_filename: frame.fileName,
--- py/src/braintrust/util.py
+++ py/src/braintrust/util.py
@@ -1,7 +1,6 @@
 import inspect
 import os.path
 import urllib.parse
-from pathlib import Path
 
 from requests import HTTPError
 
@@ -26,26 +25,18 @@ def response_raise_for_status(resp):
         raise AugmentedHTTPError(f"{resp.text}") from e
 
 
-# Taken from
-# https://stackoverflow.com/questions/3812849/how-to-check-whether-a-directory-is-a-sub-directory-of-another-directory.
-def is_subpath(test_path, directory):
-    test_path = Path(test_path)
-    directory = Path(directory)
-    return test_path == directory or directory in test_path.parents
-
-
 def get_caller_location():
     # Modified from
     # https://stackoverflow.com/questions/24438976/debugging-get-filename-and-line-number-from-which-a-function-is-called
     # to fetch the first stack frame not contained inside the same directory as
-    # this file (or any of its subdirectories).
+    # this file.
     this_dir = None
     call_stack = inspect.stack()
     for frame in call_stack:
         caller = inspect.getframeinfo(frame.frame)
         if this_dir is None:
             this_dir = os.path.dirname(caller.filename)
-        if not is_subpath(caller.filename, this_dir):
+        if os.path.dirname(caller.filename) != this_dir:
             return dict(
                 caller_functionname=caller.function,
                 caller_filename=caller.filename,
