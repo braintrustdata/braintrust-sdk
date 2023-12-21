4f1099b6ee493dc1470f27084eea6e612fe145f6
Manu Goyal <manu.goyal2013@gmail.com>
2023-12-21 11:54:59 -0800

    Simplify JS tracing API
    Analogue to https://github.com/braintrustdata/braintrust-sdk/pull/74.
    
    This is a carry-over from
    https://github.com/braintrustdata/braintrust-sdk/pull/67, with the JS
    comments addressed.

--- js/src/framework.ts
+++ js/src/framework.ts
@@ -1,14 +1,11 @@
 import chalk from "chalk";
 import {
+  NOOP_SPAN,
   Experiment,
   ExperimentSummary,
   Metadata,
   Span,
-  currentSpan,
-  noopSpan,
-  traced,
-  withCurrent,
-  withExperiment,
+  init,
 } from "./logger";
 import { Score } from "@braintrust/core";
 import { BarProgressReporter, ProgressReporter } from "./progress";
@@ -123,28 +120,28 @@ export async function Eval<Input, Output, Expected>(
 
   const progressReporter = new BarProgressReporter();
   try {
-    return await withExperiment(
-      name,
-      async (experiment) => {
-        const ret = await runEvaluator(
-          experiment,
-          {
-            evalName,
-            projectName: name,
-            ...(evaluator as Evaluator<unknown, unknown, unknown>),
-          },
-          progressReporter,
-          []
-        );
-        reportEvaluatorResult(name, ret, { verbose: true, jsonl: false });
-        return ret.summary!;
-      },
-      {
-        experiment: evaluator.experimentName,
-        metadata: evaluator.metadata,
-        isPublic: evaluator.isPublic,
-      }
-    );
+    const experiment = init(name, {
+      experiment: evaluator.experimentName,
+      metadata: evaluator.metadata,
+      isPublic: evaluator.isPublic,
+      setCurrent: false,
+    });
+    try {
+      const ret = await runEvaluator(
+        experiment,
+        {
+          evalName,
+          projectName: name,
+          ...(evaluator as Evaluator<unknown, unknown, unknown>),
+        },
+        progressReporter,
+        []
+      );
+      reportEvaluatorResult(name, ret, { verbose: true, jsonl: false });
+      return ret.summary!;
+    } finally {
+      experiment.flush();
+    }
   } finally {
     progressReporter.stop();
   }
@@ -234,33 +231,30 @@ export async function runEvaluator(
       let output: any = undefined;
       let error: unknown | undefined = undefined;
       let scores: Record<string, number> = {};
-      const callback = async () => {
+      const callback = async (rootSpan: Span) => {
         try {
           const meta = (o: Record<string, unknown>) =>
             (metadata = { ...metadata, ...o });
 
-          await traced(
-            async () => {
-              const outputResult = evaluator.task(datum.input, {
-                meta,
-                span: currentSpan(),
-              });
+          await rootSpan.traced(
+            async (span: Span) => {
+              const outputResult = evaluator.task(datum.input, { meta, span });
               if (outputResult instanceof Promise) {
                 output = await outputResult;
               } else {
                 output = outputResult;
               }
-              currentSpan().log({ input: datum.input, output });
+              span.log({ input: datum.input, output });
             },
             { name: "task" }
           );
-          currentSpan().log({ output });
+          rootSpan.log({ output });
 
           const scoringArgs = { ...datum, metadata, output };
           const scoreResults = await Promise.all(
             evaluator.scores.map(async (score, score_idx) => {
-              return traced(
-                async () => {
+              return rootSpan.traced(
+                async (span: Span) => {
                   const scoreResult = score(scoringArgs);
                   const result =
                     scoreResult instanceof Promise
@@ -271,7 +265,7 @@ export async function runEvaluator(
                     name: _,
                     ...resultRest
                   } = result;
-                  currentSpan().log({
+                  span.log({
                     output: resultRest,
                     metadata: resultMetadata,
                   });
@@ -303,7 +297,7 @@ export async function runEvaluator(
             meta({ scores: scoreMetadata });
           }
 
-          currentSpan().log({ scores, metadata });
+          rootSpan.log({ scores, metadata });
         } catch (e) {
           error = e;
         } finally {
@@ -318,19 +312,16 @@ export async function runEvaluator(
         };
       };
 
-      const rootSpan: Span = experiment
-        ? experiment.startSpan({
-            name: "eval",
-            event: {
-              input: datum.input,
-              expected: datum.expected,
-            },
-          })
-        : noopSpan;
-      try {
-        return await withCurrent(rootSpan, callback);
-      } finally {
-        rootSpan.end();
+      if (!experiment) {
+        return await callback(NOOP_SPAN);
+      } else {
+        return await experiment.traced(callback, {
+          name: "eval",
+          event: {
+            input: datum.input,
+            expected: datum.expected,
+          },
+        });
       }
     });
   const results = await Promise.all(evals);
