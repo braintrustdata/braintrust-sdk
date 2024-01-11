5975beebdcbba4fff6fa4c94377244d3494cfea1
Ankur Goyal <ankrgyl@gmail.com>
2024-01-11 14:07:54 -0800

    Small updates to core lib to include additional system fields. (#86)

--- core/js/src/db_fields.ts
+++ core/js/src/db_fields.ts
@@ -2,3 +2,12 @@ export const TRANSACTION_ID_FIELD = "_xact_id";
 export const OBJECT_DELETE_FIELD = "_object_delete";
 export const CREATED_FIELD = "created";
 export const IS_MERGE_FIELD = "_is_merge";
+export const ID_FIELD = "id";
+
+export const ALL_ROW_ID_FIELDS = [
+  TRANSACTION_ID_FIELD,
+  OBJECT_DELETE_FIELD,
+  CREATED_FIELD,
+  IS_MERGE_FIELD,
+  ID_FIELD,
+];
