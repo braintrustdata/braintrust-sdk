e846fc219adaa5c15858d442b79089aefcf4e168
Manu Goyal <manu.goyal2013@gmail.com>
2024-03-08 14:12:13 -0800

    More sophisticated batching for handling `PARENT_ID_FIELD` references
    Currently, the logic in the various loggers (both in the SDK and
    internally) assume that once you run `merge_row_batch` to consolidate log
    entries with the same ID, you can log all the rows in parallel.
    
    Since we introduced the `PARENT_ID_FIELD` attribute to allow creating a
    span underneath another row, where the reference is resolved on the
    backend, we now have a dependency between parent and child log entries,
    where the parent must be logged before the referencing child.
    
    We address this in two places: in `merge_row_batch`, we bucket all of
    the merged rows into connected components based on `PARENT_ID_FIELD`
    relationships, and then topologically sort them such that parents come
    before children in each bucket. Elements within each bucket must be
    processed in that order, while elements in different buckets can be
    logged/processed in parallel.
    
    We also rework the `batchRecords` logic to operate over this bucketed
    output of `merge_row_batch`. This is slightly tricky because it is not
    always possible to flush a full ordered bucket at once to the backend
    due to payload size constraints. So if an ordered bucket does not fit
    entirely into one batch, we must defer publishing it until we've
    published the earlier elements in the bucket. So now the batching logic
    returns a triple-nested `List[List[List[str]]]`, where the outermost
    `List[List[str]]` batch sets must be published in sequence, while each
    `List[str]` batch within a batch set can be published in parallel, and
    each `List[str]` batch individually is dependency-ordered.

--- core/js/src/merge_row_batch.ts
+++ core/js/src/merge_row_batch.ts
@@ -1,7 +1,17 @@
-import { IS_MERGE_FIELD } from "./db_fields";
-import { mergeDicts } from "./util";
+// Mirror of the functions in core/py/src/braintrust_core/merge_row_batch.py.
 
-function generateMergedRowKey(row: Record<string, unknown>) {
+import { IS_MERGE_FIELD, PARENT_ID_FIELD } from "./db_fields";
+import { mapAt, mergeDicts } from "./util";
+import {
+  AdjacencyListGraph,
+  undirectedConnectedComponents,
+  topologicalSort,
+} from "./graph_util";
+
+function generateMergedRowKey(
+  row: Record<string, unknown>,
+  useParentIdForId?: boolean
+) {
   return JSON.stringify(
     [
       "org_id",
@@ -10,30 +20,30 @@ function generateMergedRowKey(row: Record<string, unknown>) {
       "dataset_id",
       "prompt_session_id",
       "log_id",
-      "id",
+      useParentIdForId ?? false ? PARENT_ID_FIELD : "id",
     ].map((k) => row[k])
   );
 }
 
-// Mirror of core/py/src/braintrust_core/merge_row_batch.py:merge_row_batch.
 export function mergeRowBatch<
-  T extends { id: string; [IS_MERGE_FIELD]?: boolean }
->(rows: T[]): T[] {
-  const out: T[] = [];
-  const remainingRows: T[] = [];
-  // First add any rows with no ID to `out`, since they will always be
-  // independent.
+  T extends {
+    id: string;
+    [IS_MERGE_FIELD]?: boolean;
+    [PARENT_ID_FIELD]?: string;
+  }
+>(rows: T[]): T[][] {
   for (const row of rows) {
     if (row.id === undefined) {
-      out.push(row);
-    } else {
-      remainingRows.push(row);
+      throw new Error(
+        "Logged row is missing an id. This is an internal braintrust error. Please contact us at info@braintrustdata.com for help"
+      );
     }
   }
-  const rowGroups: Record<string, T> = {};
-  for (const row of remainingRows) {
+
+  const rowGroups: Map<string, T> = new Map();
+  for (const row of rows) {
     const key = generateMergedRowKey(row);
-    const existingRow = rowGroups[key];
+    const existingRow = rowGroups.get(key);
     if (existingRow !== undefined && row[IS_MERGE_FIELD]) {
       const preserveNoMerge = !existingRow[IS_MERGE_FIELD];
       mergeDicts(existingRow, row);
@@ -41,9 +51,104 @@ export function mergeRowBatch<
         delete existingRow[IS_MERGE_FIELD];
       }
     } else {
-      rowGroups[key] = row;
+      rowGroups.set(key, row);
     }
   }
-  out.push(...Object.values(rowGroups));
-  return out;
+
+  const merged = [...rowGroups.values()];
+  const rowToLabel = new Map<string, number>(
+    merged.map((r, i) => [generateMergedRowKey(r), i])
+  );
+
+  const graph: AdjacencyListGraph = new Map(
+    Array.from({ length: merged.length }).map((_, i) => [i, []])
+  );
+  merged.forEach((r, i) => {
+    const parentId = r[PARENT_ID_FIELD];
+    if (!parentId) {
+      return;
+    }
+    const parentRowKey = generateMergedRowKey(r, true /* useParentIdForId */);
+    const parentLabel = rowToLabel.get(parentRowKey);
+    if (parentLabel !== undefined) {
+      mapAt(graph, parentLabel).push(i);
+    }
+  });
+
+  const connectedComponents = undirectedConnectedComponents({
+    vertices: [...graph.keys()],
+    edges: [...graph.entries()].flatMap(([k, vs]) =>
+      vs.map((v) => [k, v] as const)
+    ),
+  });
+  const buckets = connectedComponents.map((cc) =>
+    topologicalSort(graph, cc /* visitationOrder */)
+  );
+  return buckets.map((bucket) => bucket.map((i) => merged[i]));
+}
+
+export function batchItems(args: {
+  items: string[][];
+  batchMaxNumItems?: number;
+  batchMaxNumBytes?: number;
+}): string[][][] {
+  let { items } = args;
+  const batchMaxNumItems = args.batchMaxNumItems ?? Number.POSITIVE_INFINITY;
+  const batchMaxNumBytes = args.batchMaxNumBytes ?? Number.POSITIVE_INFINITY;
+
+  const output: string[][][] = [];
+  let nextItems: string[][] = [];
+  let batchSet: string[][] = [];
+  let batch: string[] = [];
+  let batchLen = 0;
+
+  function addToBatch(item: string) {
+    batch.push(item);
+    batchLen += item.length;
+  }
+
+  function flushBatch() {
+    batchSet.push(batch);
+    batch = [];
+    batchLen = 0;
+  }
+
+  while (items.length) {
+    for (const bucket of items) {
+      let i = 0;
+      for (const item of bucket) {
+        if (
+          batch.length === 0 ||
+          (item.length + batchLen < batchMaxNumBytes &&
+            batch.length < batchMaxNumItems)
+        ) {
+          addToBatch(item);
+        } else if (i === 0) {
+          flushBatch();
+          addToBatch(item);
+        } else {
+          break;
+        }
+        ++i;
+      }
+      if (i < bucket.length) {
+        nextItems.push(bucket.slice(i));
+      }
+      if (batchLen >= batchMaxNumBytes || batch.length > batchMaxNumItems) {
+        flushBatch();
+      }
+    }
+
+    if (batch.length) {
+      flushBatch();
+    }
+    if (batchSet.length) {
+      output.push(batchSet);
+      batchSet = [];
+    }
+    items = nextItems;
+    nextItems = [];
+  }
+
+  return output;
 }
