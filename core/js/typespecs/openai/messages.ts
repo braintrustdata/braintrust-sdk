2af66bf49f9c567eeb049fa4cd7b78a5c4b32b75
Manu Goyal <manu@braintrustdata.com>
2024-04-03 12:10:47 -0700

    Also relax the openai schemas.
    They are also not really under our control.

--- core/js/typespecs/openai/messages.ts
+++ core/js/typespecs/openai/messages.ts
@@ -1,27 +1,35 @@
 import { z } from "zod";
 
-const chatCompletionSystemMessageParamSchema = z.strictObject({
-  content: z.string().default(""),
-  role: z.literal("system"),
-  name: z.string().optional(),
-});
+const chatCompletionSystemMessageParamSchema = z
+  .strictObject({
+    content: z.string().default(""),
+    role: z.literal("system"),
+    name: z.string().optional(),
+  })
+  .strip();
 
-export const chatCompletionContentPartTextSchema = z.strictObject({
-  text: z.string().default(""),
-  type: z.literal("text"),
-});
+export const chatCompletionContentPartTextSchema = z
+  .strictObject({
+    text: z.string().default(""),
+    type: z.literal("text"),
+  })
+  .strip();
 
-const imageURLSchema = z.strictObject({
-  url: z.string(),
-  detail: z
-    .union([z.literal("auto"), z.literal("low"), z.literal("high")])
-    .optional(),
-});
+const imageURLSchema = z
+  .strictObject({
+    url: z.string(),
+    detail: z
+      .union([z.literal("auto"), z.literal("low"), z.literal("high")])
+      .optional(),
+  })
+  .strip();
 
-export const chatCompletionContentPartImageSchema = z.strictObject({
-  image_url: imageURLSchema,
-  type: z.literal("image_url"),
-});
+export const chatCompletionContentPartImageSchema = z
+  .strictObject({
+    image_url: imageURLSchema,
+    type: z.literal("image_url"),
+  })
+  .strip();
 
 export const chatCompletionContentPartSchema = z.union([
   chatCompletionContentPartTextSchema,
@@ -33,47 +41,61 @@ export const chatCompletionContentSchema = z.union([
   z.array(chatCompletionContentPartSchema),
 ]);
 
-const chatCompletionUserMessageParamSchema = z.strictObject({
-  content: chatCompletionContentSchema,
-  role: z.literal("user"),
-  name: z.string().optional(),
-});
+const chatCompletionUserMessageParamSchema = z
+  .strictObject({
+    content: chatCompletionContentSchema,
+    role: z.literal("user"),
+    name: z.string().optional(),
+  })
+  .strip();
 
-const functionCallSchema = z.strictObject({
-  arguments: z.string(),
-  name: z.string(),
-});
+const functionCallSchema = z
+  .strictObject({
+    arguments: z.string(),
+    name: z.string(),
+  })
+  .strip();
 
-const functionSchema = z.strictObject({
-  arguments: z.string(),
-  name: z.string(),
-});
+const functionSchema = z
+  .strictObject({
+    arguments: z.string(),
+    name: z.string(),
+  })
+  .strip();
 
-const chatCompletionToolMessageParamSchema = z.strictObject({
-  content: z.string().default(""),
-  role: z.literal("tool"),
-  tool_call_id: z.string(),
-});
+const chatCompletionToolMessageParamSchema = z
+  .strictObject({
+    content: z.string().default(""),
+    role: z.literal("tool"),
+    tool_call_id: z.string(),
+  })
+  .strip();
 
-const chatCompletionFunctionMessageParamSchema = z.strictObject({
-  content: z.string().default(""),
-  name: z.string(),
-  role: z.literal("function"),
-});
+const chatCompletionFunctionMessageParamSchema = z
+  .strictObject({
+    content: z.string().default(""),
+    name: z.string(),
+    role: z.literal("function"),
+  })
+  .strip();
 
-const chatCompletionMessageToolCallSchema = z.strictObject({
-  id: z.string(),
-  function: functionSchema,
-  type: z.literal("function"),
-});
+const chatCompletionMessageToolCallSchema = z
+  .strictObject({
+    id: z.string(),
+    function: functionSchema,
+    type: z.literal("function"),
+  })
+  .strip();
 
-const chatCompletionAssistantMessageParamSchema = z.strictObject({
-  role: z.literal("assistant"),
-  content: z.string().nullish(),
-  function_call: functionCallSchema.optional(),
-  name: z.string().optional(),
-  tool_calls: z.array(chatCompletionMessageToolCallSchema).optional(),
-});
+const chatCompletionAssistantMessageParamSchema = z
+  .strictObject({
+    role: z.literal("assistant"),
+    content: z.string().nullish(),
+    function_call: functionCallSchema.optional(),
+    name: z.string().optional(),
+    tool_calls: z.array(chatCompletionMessageToolCallSchema).optional(),
+  })
+  .strip();
 
 export const chatCompletionMessageParamSchema = z.union([
   chatCompletionSystemMessageParamSchema,
--- core/js/typespecs/openai/tools.ts
+++ core/js/typespecs/openai/tools.ts
@@ -2,16 +2,20 @@ import { z } from "zod";
 
 export const functionParametersSchema = z.record(z.unknown());
 
-export const functionDefinitionSchema = z.strictObject({
-  name: z.string(),
-  description: z.string().optional(),
-  parameters: functionParametersSchema.optional(),
-});
+export const functionDefinitionSchema = z
+  .strictObject({
+    name: z.string(),
+    description: z.string().optional(),
+    parameters: functionParametersSchema.optional(),
+  })
+  .strip();
 
-export const chatCompletionToolSchema = z.strictObject({
-  function: functionDefinitionSchema,
-  type: z.literal("function"),
-});
+export const chatCompletionToolSchema = z
+  .strictObject({
+    function: functionDefinitionSchema,
+    type: z.literal("function"),
+  })
+  .strip();
 
 export const toolsSchema = z.array(chatCompletionToolSchema);
 export type Tools = z.infer<typeof toolsSchema>;
