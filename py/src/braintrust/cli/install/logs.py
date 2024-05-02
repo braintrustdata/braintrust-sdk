221121071e526acc4a56237d4d10161eb69e37f7
Ankur Goyal <ankrgyl@gmail.com>
2024-05-02 16:41:05 -0700

    Add JS API to logs command

--- py/src/braintrust/cli/install/logs.py
+++ py/src/braintrust/cli/install/logs.py
@@ -24,50 +24,53 @@ def main(args):
     stack = stacks[0]
     _logger.debug(stack)
 
-    log_group_name = None
+    log_group_names = []
     if args.service == "api":
-        lambda_function = [x for x in stack["Outputs"] if x["OutputKey"] == "APIHandlerName"]
-        if len(lambda_function) != 1:
-            raise ValueError(f"Expected 1 APIHandlerName, found {len(lambda_function)} ({lambda_function}))")
-        log_group_name = f"/aws/lambda/{lambda_function[0]['OutputValue']}"
+        for name in ["APIHandlerName", "APIHandlerJSName"]:
+            lambda_function = [x for x in stack["Outputs"] if x["OutputKey"] == name]
+            if len(lambda_function) > 1:
+                raise ValueError(f"Expected 1 APIHandlerName, found {len(lambda_function)} ({lambda_function}))")
+            log_group_names.append(f"/aws/lambda/{lambda_function[0]['OutputValue']}")
     else:
         raise ValueError(f"Unknown service {args.service}")
 
     start_time = int(time.time() - 3600 * args.hours) * 1000
 
-    all_streams = []
-    first_start_time = None
-    nextToken = None
+    for log_group_name in log_group_names:
+        print(f"--- LOG GROUP: {log_group_name}")
+        all_streams = []
+        first_start_time = None
+        nextToken = None
 
-    while first_start_time is None or first_start_time >= start_time:
-        kwargs = {}
-        if nextToken is not None:
-            kwargs["nextToken"] = nextToken
+        while first_start_time is None or first_start_time >= start_time:
+            kwargs = {}
+            if nextToken is not None:
+                kwargs["nextToken"] = nextToken
 
-        stream_resp = logs.describe_log_streams(logGroupName=log_group_name, descending=True, **kwargs)
+            stream_resp = logs.describe_log_streams(logGroupName=log_group_name, descending=True, **kwargs)
 
-        first_start_time = min(s["firstEventTimestamp"] for s in stream_resp["logStreams"])
-        nextToken = stream_resp.get("nextToken")
+            first_start_time = min(s["firstEventTimestamp"] for s in stream_resp["logStreams"])
+            nextToken = stream_resp.get("nextToken")
 
-        streams = [s for s in stream_resp["logStreams"] if s["firstEventTimestamp"] >= start_time]
-        streams.sort(key=lambda x: x["firstEventTimestamp"])
-        all_streams = streams + all_streams
+            streams = [s for s in stream_resp["logStreams"] if s["firstEventTimestamp"] >= start_time]
+            streams.sort(key=lambda x: x["firstEventTimestamp"])
+            all_streams = streams + all_streams
 
-    _logger.debug(all_streams)
+        _logger.debug(all_streams)
 
-    def get_events(stream):
-        return logs.get_log_events(
-            logGroupName=log_group_name,
-            logStreamName=stream["logStreamName"],
-            startTime=start_time,
-            startFromHead=True,
-        )
+        def get_events(stream):
+            return logs.get_log_events(
+                logGroupName=log_group_name,
+                logStreamName=stream["logStreamName"],
+                startTime=start_time,
+                startFromHead=True,
+            )
 
-    with ThreadPoolExecutor(8) as executor:
-        events = executor.map(get_events, all_streams)
+        with ThreadPoolExecutor(8) as executor:
+            events = executor.map(get_events, all_streams)
 
-    last_ts = None
-    for stream, log in zip(all_streams, events):
-        print(f"---- {stream['logStreamName']}")
-        for event in log["events"]:
-            print(event)
+        last_ts = None
+        for stream, log in zip(all_streams, events):
+            print(f"---- LOG STREAM: {stream['logStreamName']}")
+            for event in log["events"]:
+                print(event)
