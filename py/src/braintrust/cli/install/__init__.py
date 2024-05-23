88917cdeb6d3f13050debdf784c195b89fb49f1b
Ankur Goyal <ankrgyl@gmail.com>
2024-05-23 08:25:17 -0700

    Split out commands

--- py/src/braintrust/cli/install/__init__.py
+++ py/src/braintrust/cli/install/__init__.py
@@ -3,7 +3,7 @@ import textwrap
 
 _module_not_found_error = None
 try:
-    from . import api, logs, run_migrations
+    from . import api, bump_versions, logs, run_migrations
 except ModuleNotFoundError as e:
     _module_not_found_error = e
 
@@ -34,5 +34,5 @@ def build_parser(subparsers, parent_parser):
     else:
         install_subparsers = install_parser.add_subparsers(dest="install_subcommand", required=True)
 
-        for module in [api, logs, run_migrations]:
+        for module in [api, logs, bump_versions, run_migrations]:
             module.build_parser(install_subparsers, parents=[parent_parser])
--- py/src/braintrust/cli/install/run_migrations.py
+++ py/src/braintrust/cli/install/run_migrations.py
@@ -9,7 +9,7 @@ _logger = logging.getLogger("braintrust.install.logs")
 
 def build_parser(subparsers, parents):
     parser = subparsers.add_parser("run-migrations", help="Run schema migrations", parents=parents)
-    parser.add_argument("name", help="Name of the CloudFormation stack to collect logs from")
+    parser.add_argument("name", help="Name of the CloudFormation stack")
     parser.set_defaults(func=main)
 
 
@@ -22,22 +22,3 @@ def main(args):
     arn = migration_lambda["PhysicalResourceId"]
     lambda_ = LazyClient("lambda")
     lambda_.invoke(FunctionName=arn)
-
-    # Then, publish the API handler lambdas just to bump their environment variable values
-    api_handler = [r for r in resources["StackResources"] if r["LogicalResourceId"] == "APIHandler"]
-    api_handler_js = [r for r in resources["StackResources"] if r["LogicalResourceId"] == "APIHandlerJS"]
-
-    if not api_handler or not api_handler_js:
-        raise ValueError("No APIHandler or APIHandlerJS found in the stack.")
-
-    api_handler = api_handler[0]
-    api_handler_js = api_handler_js[0]
-
-    # Publish a new version of the API handler and re-point the "live2" alias to it
-    for resource, alias in [(api_handler, "live2"), (api_handler_js, "live")]:
-        new_version = lambda_.publish_version(FunctionName=resource["PhysicalResourceId"])
-        lambda_.update_alias(
-            FunctionName=resource["PhysicalResourceId"],
-            Name=alias,
-            FunctionVersion=new_version["Version"],
-        )
