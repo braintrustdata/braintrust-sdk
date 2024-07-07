d8e8ea98ba2e2a103c17d5f81bdd8b7d02df8f1a
Ankur Goyal <ankrgyl@gmail.com>
2024-07-07 11:49:21 -0700

    Add stream docs in python

--- py/src/braintrust/functions/stream.py
+++ py/src/braintrust/functions/stream.py
@@ -1,3 +1,10 @@
+"""
+This module provides classes and functions for handling Braintrust streams.
+
+A Braintrust stream is a wrapper around a generator of `BraintrustStreamChunk`,
+with utility methods to make them easy to log and convert into various formats.
+"""
+
 import dataclasses
 import json
 from itertools import tee
@@ -8,12 +15,20 @@ from sseclient import SSEClient
 
 @dataclasses.dataclass
 class BraintrustTextChunk:
+    """
+    A chunk of text data from a Braintrust stream.
+    """
+
     data: str
     type: Literal["text_delta"] = "text_delta"
 
 
 @dataclasses.dataclass
 class BraintrustJsonChunk:
+    """
+    A chunk of JSON data from a Braintrust stream.
+    """
+
     data: str
     type: Literal["json_delta"] = "json_delta"
 
@@ -22,7 +37,18 @@ BraintrustStreamChunk = Union[BraintrustTextChunk, BraintrustJsonChunk]
 
 
 class BraintrustStream:
+    """
+    A Braintrust stream. This is a wrapper around a generator of `BraintrustStreamChunk`,
+    with utility methods to make them easy to log and convert into various formats.
+    """
+
     def __init__(self, base_stream: Union[SSEClient, List[BraintrustStreamChunk]]):
+        """
+        Initialize a BraintrustStream.
+
+        Args:
+            base_stream: Either an SSEClient or a list of BraintrustStreamChunks.
+        """
         if isinstance(base_stream, SSEClient):
             self.stream = self._parse_sse_stream(base_stream)
         else:
@@ -30,6 +56,15 @@ class BraintrustStream:
         self._memoized_final_value = None
 
     def _parse_sse_stream(self, sse_client: SSEClient) -> Generator[BraintrustStreamChunk, None, None]:
+        """
+        Parse an SSE stream into BraintrustStreamChunks.
+
+        Args:
+            sse_client: The SSEClient to parse.
+
+        Yields:
+            BraintrustStreamChunk: Parsed chunks from the SSE stream.
+        """
         for event in sse_client.events():
             if event.event == "text_delta":
                 yield BraintrustTextChunk(data=json.loads(event.data))
@@ -37,20 +72,54 @@ class BraintrustStream:
                 yield BraintrustJsonChunk(data=event.data)
 
     def copy(self):
+        """
+        Copy the stream. This returns a new stream that shares the same underlying
+        generator (via `tee`). Since generators are consumed in Python, use `copy()` if you
+        need to use the stream multiple times.
+
+        Returns:
+            BraintrustStream: A new stream that you can independently consume.
+        """
         current_stream = self.stream
         self.stream, new_stream = tee(current_stream)
         return BraintrustStream(new_stream)
 
     def final_value(self):
+        """
+        Get the final value of the stream. This will return the final value of the stream
+        when it is fully consumed. Multiple calls to `final_value()` will return the same value,
+        so it is safe to call this multiple times.
+
+        This function consumes the stream, so if you need to use the stream multiple
+        times, you should call `copy()` first.
+
+        Returns:
+            The final value of the stream.
+        """
         if self._memoized_final_value is None:
             self._memoized_final_value = parse_stream(self)
         return self._memoized_final_value
 
     def __iter__(self):
+        """
+        Iterate over the stream chunks.
+
+        Yields:
+            BraintrustStreamChunk: The next chunk in the stream.
+        """
         yield from self.stream
 
 
 def parse_stream(stream: BraintrustStream):
+    """
+    Parse a BraintrustStream into its final value.
+
+    Args:
+        stream: The BraintrustStream to parse.
+
+    Returns:
+        The final value of the stream.
+    """
     text_chunks = []
     json_chunks = []
 
