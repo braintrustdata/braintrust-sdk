f870e42d0c37b69820953807f534da0e5f403522
Ankur Goyal <ankrgyl@gmail.com>
2024-09-21 10:32:32 -0700

    Improve code preview semantics

--- js/src/functions/infer-source.ts
+++ js/src/functions/infer-source.ts
@@ -1,9 +1,10 @@
-import { MappingItem, SourceMapConsumer } from "source-map";
+import { SourceMapConsumer } from "source-map";
 import * as fs from "fs/promises";
 import { EvaluatorFile, warning } from "../framework";
 import { loadModule } from "./load-module";
 import { CodeBundle } from "@braintrust/core/typespecs/dist";
 import path from "path";
+import type { Node } from "typescript";
 
 interface SourceMapContext {
   inFiles: Record<string, string[]>;
@@ -125,97 +126,65 @@ export async function findCodeDefinition({
   }
 
   const originalLines = inFiles[originalPosition.source];
-  const endMapping = findNextMapping(
-    sourceMap,
+
+  // Parse the file with Typescript to find the function definition
+  const ts = await getTsModule();
+  if (!ts) {
+    return undefined;
+  }
+  const sourceFile = ts.createSourceFile(
     originalPosition.source,
-    originalPosition.line,
-    originalPosition.column,
-    originalLines,
+    originalLines.join("\n"),
+    ts.ScriptTarget.Latest,
+    true,
+  );
+  let functionNode: Node | undefined = undefined;
+  const targetPosition = ts.getPositionOfLineAndCharacter(
+    sourceFile,
+    originalPosition.line - 1,
+    originalPosition.column || 0,
   );
 
-  const ret = extractFunctionDefinition({
-    start: {
-      line: originalPosition.line,
-      column: originalPosition.column,
-    },
-    end: endMapping
-      ? {
-          line: endMapping.originalLine,
-          column: endMapping.originalColumn,
-        }
-      : null,
-    lines: originalLines,
-  });
-  return ret.length === 0 ? undefined : ret.slice(0, 10240);
-}
-
-export function extractFunctionDefinition({
-  start,
-  end,
-  lines,
-}: {
-  start: { line: number; column: number | null };
-  end: { line: number; column: number | null } | null;
-  lines: string[];
-}) {
-  // Extract the function definition
-  let functionDefinition = "";
-  for (let i = start.line - 1; i < lines.length; i++) {
-    let line = lines[i];
-    if (end && end.column !== null && i === end.line - 1) {
-      line = line.slice(0, end.column + 1);
-    } else if (
-      start.column !== null &&
-      start.column > 0 &&
-      i === start.line - 1
-    ) {
-      line = line.slice(start.column - 1);
-    } else if (end !== null && i >= end.line) {
-      break;
+  ts.forEachChild(sourceFile, function visit(node) {
+    if (node.pos <= targetPosition && targetPosition < node.end) {
+      if (
+        ts.isFunctionDeclaration(node) ||
+        ts.isFunctionExpression(node) ||
+        ts.isArrowFunction(node)
+      ) {
+        functionNode = node;
+      } else {
+        ts.forEachChild(node, visit);
+      }
     }
+  });
 
-    functionDefinition += line + "\n";
+  if (!functionNode) {
+    return undefined;
   }
 
-  return functionDefinition.trim();
+  const printer = ts.createPrinter();
+  const functionDefinition = printer.printNode(
+    ts.EmitHint.Unspecified,
+    functionNode,
+    sourceFile,
+  );
+
+  return functionDefinition;
 }
 
-// Add this new function to find the next mapping
-function findNextMapping(
-  sourceMap: SourceMapConsumer,
-  source: string,
-  line: number,
-  column: number | null,
-  lines: string[],
-): MappingItem | null {
-  let nextMapping: MappingItem | null = null;
-  let finished = false;
-  sourceMap.eachMapping(
-    (mapping: MappingItem) => {
-      if (mapping.source !== source || finished) {
-        return;
-      }
-      if (
-        mapping.originalLine > line &&
-        mapping.originalColumn <= (column ?? 0)
-      ) {
-        if (
-          nextMapping &&
-          lines[mapping.originalLine - 1][mapping.originalColumn] !== "}"
-        ) {
-          // If we've already collected a }, and we encounter a non-}, then we've found
-          // the end of the function definition. In the worst case, this will result in
-          // two functions.
-          finished = true;
-        } else if (
-          lines[mapping.originalLine - 1][mapping.originalColumn] === "}"
-        ) {
-          nextMapping = mapping;
-        }
-      }
-    },
-    undefined,
-    SourceMapConsumer.ORIGINAL_ORDER,
-  );
-  return nextMapping;
+let tsModule: typeof import("typescript") | undefined = undefined;
+async function getTsModule() {
+  if (!tsModule) {
+    try {
+      tsModule = await import("typescript");
+    } catch (e) {
+      console.warn(
+        warning(
+          "Failed to load TypeScript module. Will not use Typescript to derive previe.",
+        ),
+      );
+    }
+  }
+  return tsModule;
 }
