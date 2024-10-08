1d1b3f1eeaef31d02e8c9c86ecf9f6a45b227a8c
Kevin Chen <kevin@kevinchen.co>
2024-10-07 19:30:07 -0700

    some parameters are shared with g711

--- core/js/typespecs/audio.ts
+++ core/js/typespecs/audio.ts
@@ -1,25 +1,35 @@
 import { z } from "zod";
 
 export const audioJsonSchema = z.object({
-  audio_codec: z.discriminatedUnion("name", [
-    z.object({
-      name: z.literal("pcm"),
-      channels: z.number().nonnegative().int(),
-      sample_rate: z.number().nonnegative().int(),
-      bits_per_sample: z.number().nonnegative().int(),
-      byte_order: z.enum(["little", "big"]).default("little"),
-      number_encoding: z.enum(["int", "float"]).default("int"),
-    }),
-    z.object({
-      name: z.literal("g711"),
-    }),
-  ]),
-  buffers: z.array(z.string()),
+  audio_codec: z
+    .discriminatedUnion("name", [
+      z.object({
+        name: z.literal("pcm"),
+        byte_order: z.enum(["little", "big"]).default("little"),
+        number_encoding: z.enum(["int", "float"]).default("int"),
+      }),
+      z.object({
+        name: z.literal("g711"),
+        algorithm: z.enum(["a", "mu"]),
+      }),
+    ])
+    .and(
+      z.object({
+        // Common codec parameters.
+        channels: z.number().nonnegative().int(),
+        sample_rate: z.number().nonnegative().int(),
+        bits_per_sample: z.number().nonnegative().int(),
+      }),
+    ),
+
+  // Actual audio data as base64. May be split over multiple, individually encoded base64 strings to allow incremental encoding.
+  buffers: z.array(z.string().describe("base64")),
 });
 
 export type AudioJson = z.infer<typeof audioJsonSchema>;
 
-export type AudioBufferType = AudioJson["audio_codec"]["name"];
+export type AudioCodec = AudioJson["audio_codec"];
+export type AudioCodecType = AudioCodec["name"];
 
 export const attachmentSchema = z.object({
   attachments: z.object({
