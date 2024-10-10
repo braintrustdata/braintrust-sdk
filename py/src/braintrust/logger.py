braintrustdata/braintrust-sdk

commit b4f629bddb661422c15c4b0459104b6a6387f3a1

MetricSummary print token counts without decimals. (#376)

py/src/braintrust/logger.py
@@ -3154,7 +3154,7 @@ class MetricSummary(SerializableDataClass):
     # Used to help with formatting
     _longest_metric_name: int
 
-    metric: float
+    metric: float | int
     """Average metric across all examples."""
     unit: str
     """Unit label for the metric."""
@@ -3166,8 +3166,8 @@ class MetricSummary(SerializableDataClass):
     """Difference in metric between the current and reference experiment."""
 
     def __str__(self):
-        # format with 2 decimal points
-        metric = f"{self.metric:.2f}"
+        number_fmt = "{:d}" if isinstance(self.metric, int) else "{:.2f}"
+        metric = number_fmt.format(self.metric)
         if self.diff is None:
             return textwrap.dedent(f"""{metric}{self.unit} {self.name}""")
 

js/src/framework.ts
@@ -981,11 +981,9 @@ function formatMetricSummary(
   summary: MetricSummary,
   longestMetricName: number,
 ) {
-  const diffString = isEmpty(summary.diff)
-    ? ""
-    : ` (${summary.diff > 0 ? "+" : ""}${(summary.diff * 100).toFixed(2)}%)`;
+  const fractionDigits = Number.isInteger(summary.metric) ? 0 : 2;
   const metricName = `'${summary.name}'`.padEnd(longestMetricName + 2);
-  return `${summary.metric.toFixed(2)}${summary.unit} ${metricName}\t(${
+  return `${summary.metric.toFixed(fractionDigits)}${summary.unit} ${metricName}\t(${
     summary.improvements
   } improvements, ${summary.regressions} regressions)`;
 }

commit 29d34846c0b6800acceb91dcbc17a596eda7a707

Python: Use union type compatible with python3.9 and older.
I introduced this regression in https://github.com/braintrustdata/braintrust-sdk/pull/376

py/src/braintrust/logger.py
@@ -3161,7 +3161,7 @@ class MetricSummary(SerializableDataClass):
     # Used to help with formatting
     _longest_metric_name: int
 
-    metric: float | int
+    metric: Union[float, int]
     """Average metric across all examples."""
     unit: str
     """Unit label for the metric."""