4e1b9356351711bd11543bd479cafa0da8062ae9
Austin Moehle <frozenvapor100@gmail.com>
2024-10-13 16:51:25 -0700

    Change to parent string

--- core/js/src/http_headers.ts
+++ core/js/src/http_headers.ts
@@ -8,5 +8,5 @@ export const BT_CURSOR_HEADER = "x-bt-cursor";
 // User impersonation header.
 export const BT_IMPERSONATE_USER = "x-bt-impersonate-user";
 
-// Project ID header for OTEL exporters to specify a destination Braintrust project.
-export const BT_PROJECT_ID = "x-bt-project-id";
+// Parent header for OTEL exporters.
+export const BT_PARENT = "x-bt-parent";
--- py/src/braintrust/http_headers.py
+++ py/src/braintrust/http_headers.py
@@ -1,4 +1,4 @@
 BT_FOUND_EXISTING_HEADER = "x-bt-found-existing"
 BT_CURSOR_HEADER = "x-bt-cursor"
 BT_IMPERSONATE_USER = "x-bt-impersonate-user"
-BT_PROJECT_ID = "x-bt-project-id"
+BT_PARENT = "x-bt-parent"
