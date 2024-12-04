966b9c86680bdeba63ee8a21ab4c282ec3d6c9c2
Olmo Maldonado <olmo.maldonado@gmail.com>
2024-12-04 13:11:24 -0600

    add initial tests for BraintrustCallbackHandler

---
+++ integrations/langchain-js/src/BraintrustCallbackHandler.test.ts
@@ -0,0 +1,99 @@
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { ChatOpenAI } from "@langchain/openai";
+import { initLogger } from "braintrust";
+import { http, HttpResponse } from "msw";
+import { describe, expect, it } from "vitest";
+import { BraintrustCallbackHandler as BaseBraintrustCallbackHandler } from "./BraintrustCallbackHandler";
+import { server } from "./test/setup";
+
+initLogger({
+  projectName: "langchain",
+});
+
+class BraintrustCallbackHandler extends BaseBraintrustCallbackHandler {
+  public stack: string[] = [];
+  public position: number = 0;
+
+  protected startSpan(
+    ...args: Parameters<BaseBraintrustCallbackHandler["startSpan"]>
+  ) {
+    super.startSpan(...args);
+
+    this.stack.push(args[0].runId);
+    this.position++;
+  }
+
+  protected endSpan(
+    ...args: Parameters<BaseBraintrustCallbackHandler["endSpan"]>
+  ): void {
+    super.endSpan(...args);
+
+    this.stack.pop();
+    this.position--;
+  }
+}
+
+const handler = new BraintrustCallbackHandler();
+
+describe("BraintrustCallbackHandler", () => {
+  it("should handle LLM calls", async () => {
+    server.use(
+      http.post("https://api.openai.com/v1/chat/completions", () => {
+        return HttpResponse.json({
+          id: "chatcmpl-Aao716hWOt9HBihjWh9iAPGWRpkFd",
+          object: "chat.completion",
+          created: 1733335803,
+          model: "gpt-4o-mini-2024-07-18",
+          choices: [
+            {
+              index: 0,
+              message: {
+                role: "assistant",
+                content: "1 + 2 equals 3.",
+                refusal: null,
+              },
+              logprobs: null,
+              finish_reason: "stop",
+            },
+          ],
+          usage: {
+            prompt_tokens: 15,
+            completion_tokens: 8,
+            total_tokens: 23,
+            prompt_tokens_details: {
+              cached_tokens: 0,
+              audio_tokens: 0,
+            },
+            completion_tokens_details: {
+              reasoning_tokens: 0,
+              audio_tokens: 0,
+              accepted_prediction_tokens: 0,
+              rejected_prediction_tokens: 0,
+            },
+          },
+          system_fingerprint: "fp_0705bf87c0",
+        });
+      }),
+
+      http.post("*/logs", async () => {
+        return HttpResponse.json(["4bc6305f-2175-4481-bc84-7c55a456b7ea"]);
+      }),
+    );
+
+    const prompt = ChatPromptTemplate.fromTemplate(`What is 1 + {number}?`);
+    const model = new ChatOpenAI({
+      model: "gpt-4o-mini",
+    });
+
+    const chain = prompt.pipe(model);
+
+    const message = await chain.invoke(
+      { number: "2" },
+      { callbacks: [handler] },
+    );
+
+    expect(handler.stack.length).toBe(0);
+
+    expect(message.content).toBe("1 + 2 equals 3.");
+  });
+});
