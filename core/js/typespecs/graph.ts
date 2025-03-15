5cbff2692c66774c7fee12dd4f946ae7dad77acd
Ankur Goyal <ankrgyl@gmail.com>
2025-03-15 14:11:04 -0700

    more

---
+++ core/js/typespecs/graph.ts
@@ -0,0 +1,51 @@
+import { z } from "zod";
+import { functionIdSchema } from "./functions";
+
+export const nodeDataSchema = z.union([
+  z.object({
+    type: z.literal("function"),
+    function: functionIdSchema,
+  }),
+  z.object({
+    type: z.literal("input").describe("The input to the graph"),
+  }),
+  z.object({
+    type: z.literal("output").describe("The output of the graph"),
+  }),
+  z.object({
+    type: z.literal("literal"),
+    value: z.unknown().describe("A literal value to be returned"),
+  }),
+  z.object({
+    type: z.literal("if"),
+  }),
+]);
+
+export const ifNodeParametersSchema = z.object({
+  condition: z.string().describe("A BTQL expression to evaluate"),
+  then: z
+    .string()
+    .describe("The id of the node to execute if the condition is true"),
+  else: z
+    .string()
+    .describe("The id of the node to execute if the condition is false"),
+});
+
+export const graphNodeSchema = z.object({
+  id: z.string().uuid().describe("The id of the node"),
+  data: nodeDataSchema,
+  description: z.string().describe("The description of the node"),
+});
+
+export const graphDataSchema = z.object({
+  nodes: z.array(graphNodeSchema),
+  edges: z.array(
+    z.object({
+      source: z.string().uuid().describe("The id of the source node"),
+      target: z.string().uuid().describe("The id of the target node"),
+      variable: z.string().describe("The variable name for the edge"),
+    }),
+  ),
+});
+
+export type GraphData = z.infer<typeof graphDataSchema>;
