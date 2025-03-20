c9e3d01a3b974ca7e6958eac4c8e16787d78c70a
Matt Perpick <matthewperpick@gmail.com>
2025-03-20 15:06:18 -0700

    Add a little app to test anthropic tracing.

--- py/examples/anthropic_app.py
+++ py/examples/anthropic_app.py
@@ -1,17 +1,29 @@
-#!/usr/bin/env uv run --script
-# /// script
-# dependencies = [
-#   "anthropic",
-# ]
-# ///
+#!/usr/bin/env python
+
+
+# ASDF !/usr/bin/env uv run --script
+# ASDF  /// script
+# ASDF  dependencies = [
+# ASDF    "anthropic",
+# ASDF    "braintrust",
+# ASDF  ]
+# ASDF  ///
 
 
 import os
+import random
+import time
 
 import anthropic
+import braintrust
+
+print("BRAINTRUST CLIENT", braintrust.__file__)
 
 # Initialize Anthropic client (needs ANTHROPIC_API_KEY) (needs
 client = anthropic.Anthropic()
+
+braintrust.init_logger(project="test-anthropic-app")
+
 
 # List of questions to ask
 questions = [
@@ -20,12 +32,30 @@ questions = [
     "What are three interesting facts about octopuses?",
 ]
 
-# Ask each question and display the response
-for i, question in enumerate(questions):
-    print(f"\nQuestion {i+1}: {question}")
 
-    message = client.messages.create(
+@braintrust.traced
+def _ask_anthropic(question):
+    time.sleep(random.random())
+    return "TODO"
+
+    msg = client.messages.create(
         model="claude-3-haiku-20240307", max_tokens=300, messages=[{"role": "user", "content": question}]
     )
+    return msg.content[0].text
 
-    print(f"Answer: {message.content[0].text}")
+
+@braintrust.traced
+def ask():
+    # Ask each question and display the response
+    for i, question in enumerate(questions):
+        _ask_anthropic(question)
+
+
+def main():
+    while True:
+        ask()
+        time.sleep(3)
+
+
+if __name__ == "__main__":
+    main()
