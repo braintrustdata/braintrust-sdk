1ee4211e5bc419b24309bccc29d84c71d8153e33
Matt Perpick <matthewperpick@gmail.com>
2025-03-27 14:10:46 -0700

    anthropic: ensure the library runs without anthropic (#610)
    - `wrap_anthropic` would fail if anthropic wasn't installed
    -  fixed the nox tests (which weren't quite running properly)
    - added some tests to ensure our library can always be imported and run
    core tests without extra dependencies

--- py/noxfile.py
+++ py/noxfile.py
@@ -3,25 +3,49 @@ Define our tests to run against different combinations of
 Python & library versions.
 """
 
-
 import nox
 
 LATEST = "__latest__"
 SRC = "src/braintrust"
 
 
+ERROR_CODES = tuple(range(1, 256))
+
+# List your package here if it's not guaranteed to be installed. We'll (try to)
+# validate things work with or without them.
+VENDOR_PACKAGES = ("anthropic", "openai")
+
+
 ANTHROPIC_VERSIONS = ("0.48.0", "0.49.0", LATEST)
+
+
+@nox.session()
+def test_no_deps(session):
+    """Ensure that with no dependencies, we can still import and use the
+    library.
+    """
+    _install_test_deps(session)
+
+    # verify we haven't installed our 3p deps.
+    for p in VENDOR_PACKAGES:
+        session.run("python", "-c", f"import {p}", success_codes=ERROR_CODES)
+
+    session.run("python", "-c", "import braintrust")
+    session.run("pytest", SRC, f"--ignore={SRC}/wrappers")
 
 
 @nox.session()
 @nox.parametrize("version", ANTHROPIC_VERSIONS)
 def test_anthropic(session, version):
-    """Run pytest against a specific version of the anthropic SDK."""
-
+    _install_test_deps(session)
     _install(session, "anthropic", version)
+    session.run("pytest", f"{SRC}/wrappers/test_anthropic.py")
 
-    # Run your tests
-    session.run("pytest", f"{SRC}/wrappers/test_anthropic.py", external=True)
+
+def _install_test_deps(session):
+    session.install("pytest")
+    session.install("pytest-asyncio")
+    session.install("-e", ".[test]")
 
 
 def _install(session, package, version=LATEST):
