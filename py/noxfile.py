be35383f099a8bb91334fdd73f06ba135b72e64c
Matt Perpick <matt@braintrustdata.com>
2025-04-07 11:52:10 -0400

    Build, template and test the wheel in SDK.

--- py/noxfile.py
+++ py/noxfile.py
@@ -3,13 +3,22 @@ Define our tests to run against different combinations of
 Python & library versions.
 """
 
+import glob
+
 import nox
 
 LATEST = "__latest__"
 SRC = "src/braintrust"
+DIST = "dist"
 
 
 ERROR_CODES = tuple(range(1, 256))
+
+# The source of the braintrust package. We can test it against source code
+# or a built wheel. Run `nox -k code` for your normal dev flow.
+CODE = "code"
+WHEEL = "wheel"
+BT_SOURCES = (CODE, WHEEL)
 
 # List your package here if it's not guaranteed to be installed. We'll (try to)
 # validate things work with or without them.
@@ -21,11 +30,20 @@ OPENAI_VERSIONS = (LATEST, "1.71")
 
 
 @nox.session()
-def test_no_deps(session):
+@nox.parametrize("source", BT_SOURCES)
+def test_no_deps(session, source):
     """Ensure that with no dependencies, we can still import and use the
     library.
     """
-    _install_test_deps(session)
+    _install_test_deps(session, source)
+
+    if source == WHEEL:
+        # sanity check we have installed from the wheel.
+        lines = [
+            "import sys, braintrust as b",
+            "sys.exit(0 if 'site-packages' in b.__file__ else 1)",
+        ]
+        session.run("python", "-c", ";".join(lines))
 
     # verify we haven't installed our 3p deps.
     for p in VENDOR_PACKAGES:
@@ -37,24 +55,42 @@ def test_no_deps(session):
 
 @nox.session()
 @nox.parametrize("version", ANTHROPIC_VERSIONS)
-def test_anthropic(session, version):
-    _install_test_deps(session)
+@nox.parametrize("source", BT_SOURCES)
+def test_anthropic(session, source, version):
+    _install_test_deps(session, source)
     _install(session, "anthropic", version)
     session.run("pytest", f"{SRC}/wrappers/test_anthropic.py")
 
 
 @nox.session()
 @nox.parametrize("version", OPENAI_VERSIONS)
+@nox.parametrize("source", BT_SOURCES)
 def test_openai(session, version):
-    _install_test_deps(session)
+    _install_test_deps(session, source)
     _install(session, "openai", version)
     session.run("pytest", f"{SRC}/wrappers/test_openai.py")
 
 
-def _install_test_deps(session):
+def _install_test_deps(session, source):
     session.install("pytest")
     session.install("pytest-asyncio")
-    session.install("-e", ".[test]")
+
+    if source == CODE:
+        session.install("-e", ".[test]")
+    elif source == WHEEL:
+        wheel_path = _get_braintrust_wheel()
+        session.install(wheel_path)
+    else:
+        raise Exception(f"Invalid source: {source}")
+
+
+def _get_braintrust_wheel():
+    path = "dist/braintrust-*.whl"
+    wheels = glob.glob(path)
+    if len(wheels) != 1:
+        msg = f"There should be one wheel in {path}. Got {len(wheels)}"
+        raise Exception(msg)
+    return wheels[0]
 
 
 def _install(session, package, version=LATEST):
