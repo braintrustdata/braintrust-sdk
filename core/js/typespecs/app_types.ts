90a76730c4ccc74d3cce95abfff445d70bb98499
Hurshal Patel <hurshal@braintrustdata.com>
2025-06-07 18:46:40 +0100

    btql export automation (#715)
    Co-authored-by: Ankur Goyal <ankrgyl@gmail.com>

--- core/js/typespecs/app_types.ts
+++ core/js/typespecs/app_types.ts
@@ -10,6 +10,11 @@ import { viewDataSchema, viewOptionsSchema, viewTypeEnum } from "./view";
 import { functionDataSchema, functionTypeEnum } from "./functions";
 import { savedFunctionIdSchema } from "./function_id";
 import { repoInfoSchema } from "./git_types";
+import {
+  automationConfigSchema,
+  btqlExportAutomationConfigSchema,
+  logAutomationConfigSchema,
+} from "./automations";
 extendZodWithOpenApi(z);
 
 // Section: App DB table schemas
@@ -571,37 +576,26 @@ export const projectAutomationSchema = z
     created: projectAutomationBaseSchema.shape.created,
     name: projectAutomationBaseSchema.shape.name,
     description: projectAutomationBaseSchema.shape.description,
-    config: z
-      .object({
-        event_type: z
-          .enum(["logs"])
-          .describe("The event which starts the automation execution"),
-        btql_filter: z
-          .string()
-          .describe("BTQL filter to identify rows for the automation rule"),
-        interval_seconds: z
-          .number()
-          .min(1)
-          .max(30 * 24 * 60 * 60)
-          .describe(
-            "Perform the triggered action at most once in this interval of seconds",
-          ),
-        action: z
-          .object({
-            type: z
-              .enum(["webhook"])
-              .describe(
-                "The type of action to take when the automation rule is triggered",
-              ),
-            url: z.string().describe("The webhook URL to send the request to"),
-          })
-          .describe("The action to take when the automation rule is triggered"),
-      })
-      .describe("The configuration for the automation rule"),
+    config: automationConfigSchema.describe(
+      "The configuration for the automation rule",
+    ),
   })
   .openapi("ProjectAutomation");
 
 export type ProjectAutomation = z.infer<typeof projectAutomationSchema>;
+export const logAutomationSchema = projectAutomationSchema.merge(
+  z.object({
+    config: logAutomationConfigSchema,
+  }),
+);
+export type LogAutomation = z.infer<typeof logAutomationSchema>;
+
+export const btqlExportAutomationSchema = projectAutomationSchema.merge(
+  z.object({
+    config: btqlExportAutomationConfigSchema,
+  }),
+);
+export type BtqlExportAutomation = z.infer<typeof btqlExportAutomationSchema>;
 
 export const onlineScoreConfigSchema = z
   .object({
