ff0a43135cf39051503d824dc37e9a10dc130906
Matt Perpick <matt@braintrustdata.com>
2025-06-26 13:31:49 -0400

    clean up

--- js/scripts/validate-release.sh
+++ js/scripts/validate-release.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-# Script to validate our release code
-# - Ensures we're releasing from the main branch
-# - The git status is clean
-
+# This script attempts to verify the state of the repo is a candidate for
+# release and will fail if it is not.
 set -e
 
+
+
+# Ensure the current branch has been pushed to main (aka tests have passed)
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 if [ "$CURRENT_BRANCH" != "main" ]; then
-  # Ensure we have main branch history
   git fetch origin main --depth=1000
-
   # assert this commit is on the main branch
   if ! git merge-base --is-ancestor "$(git rev-parse HEAD)" origin/main; then
     echo "ERROR: Current commit is not on the main branch"
     exit 1
-  else
-    echo "branch is on main"
   fi
 fi
 
+
+# Assert we aren't releasing any uncommitted code
 if [ -n "$(git status --porcelain)" ]; then
   echo "Error: working tree is not clean"
   exit 1
