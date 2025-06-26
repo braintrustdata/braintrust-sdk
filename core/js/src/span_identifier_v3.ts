9a7ab94317edf25a6360352fe6851af16af11977
Ankur Goyal <ankrgyl@gmail.com>
2025-06-25 19:16:34 -0700

    refactor

--- core/js/src/span_identifier_v3.ts
+++ core/js/src/span_identifier_v3.ts
@@ -9,6 +9,13 @@ import {
 import { SpanComponentsV2 } from "./span_identifier_v2";
 import { z } from "zod";
 import { InvokeFunctionRequest } from "typespecs";
+import {
+  base64ToUint8Array,
+  concatUint8Arrays,
+  stringToUint8Array,
+  uint8ArrayToBase64,
+  uint8ArrayToString,
+} from "./bytes";
 
 function tryMakeUuid(
   s: string,
@@ -22,44 +29,6 @@ function tryMakeUuid(
   } catch {
     return { bytes: undefined, isUUID: false };
   }
-}
-
-function concatUint8Arrays(...arrays: Uint8Array[]): Uint8Array {
-  const totalLength = arrays.reduce((acc, arr) => acc + arr.length, 0);
-  const result = new Uint8Array(totalLength);
-  let offset = 0;
-  for (const arr of arrays) {
-    result.set(arr, offset);
-    offset += arr.length;
-  }
-  return result;
-}
-
-function uint8ArrayToBase64(uint8Array: Uint8Array): string {
-  let binary = "";
-  for (let i = 0; i < uint8Array.length; i++) {
-    binary += String.fromCharCode(uint8Array[i]);
-  }
-  return btoa(binary);
-}
-
-function base64ToUint8Array(base64: string): Uint8Array {
-  const binary = atob(base64);
-  const uint8Array = new Uint8Array(binary.length);
-  for (let i = 0; i < binary.length; i++) {
-    uint8Array[i] = binary.charCodeAt(i);
-  }
-  return uint8Array;
-}
-
-function uint8ArrayToString(uint8Array: Uint8Array): string {
-  const decoder = new TextDecoder("utf-8");
-  return decoder.decode(uint8Array);
-}
-
-function stringToUint8Array(str: string): Uint8Array {
-  const encoder = new TextEncoder();
-  return encoder.encode(str);
 }
 
 const ENCODING_VERSION_NUMBER = 3;
