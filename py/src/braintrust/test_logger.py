430e3379089249b06716099561cf952dc94578b9
Olmo Maldonado <olmo@braintrust.dev>
2025-07-24 11:16:58 -0500

    cleanup test_logger and avoid fixures from getting auto removed by ruff

--- py/src/braintrust/test_logger.py
+++ py/src/braintrust/test_logger.py
@@ -1,5 +1,4 @@
 import asyncio
-import inspect
 import os
 import time
 from typing import AsyncGenerator, List
@@ -15,10 +14,10 @@ from braintrust.test_helpers import (
     assert_dict_matches,
     assert_logged_out,
     init_test_logger,
-    simulate_login,
+    simulate_login,  # noqa # pyright: ignore
     simulate_logout,
-    with_memory_logger,
-    with_simulate_login,
+    with_memory_logger,  # noqa # pyright: ignore
+    with_simulate_login,  # noqa # pyright: ignore
 )
 
 
