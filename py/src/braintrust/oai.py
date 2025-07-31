d787671565b9b722a83f890ef3d7293aec9e239d
Hurshal Patel <hurshal@braintrustdata.com>
2025-07-31 16:41:03 -0700

    remove with_raw_response

--- py/src/braintrust/oai.py
+++ py/src/braintrust/oai.py
@@ -663,13 +663,7 @@ class ResponsesV1Wrapper(NamedWrapper):
         return ResponseWrapper(self.__responses.with_raw_response.create, None).create(*args, **kwargs)
 
     def parse(self, *args: Any, **kwargs: Any) -> Any:
-        # Try to use with_raw_response.parse if it exists (newer SDK versions)
-        # Otherwise fall back to direct parse call
-        if hasattr(self.__responses.with_raw_response, "parse"):
-            return ResponseWrapper(self.__responses.with_raw_response.parse, None).create(*args, **kwargs)
-        else:
-            # Older SDK versions - parse doesn't have a with_raw_response equivalent
-            return ResponseWrapper(self.__responses.parse, None).create(*args, **kwargs)
+        return ResponseWrapper(self.__responses.parse, None).create(*args, **kwargs)
 
 
 class AsyncResponsesV1Wrapper(NamedWrapper):
@@ -682,13 +676,7 @@ class AsyncResponsesV1Wrapper(NamedWrapper):
         return AsyncResponseWrapper(response)
 
     async def parse(self, *args: Any, **kwargs: Any) -> Any:
-        # Try to use with_raw_response.parse if it exists (newer SDK versions)
-        # Otherwise fall back to direct parse call
-        if hasattr(self.__responses.with_raw_response, "parse"):
-            response = await ResponseWrapper(None, self.__responses.with_raw_response.parse).acreate(*args, **kwargs)
-        else:
-            # Older SDK versions - parse doesn't have a with_raw_response equivalent
-            response = await ResponseWrapper(None, self.__responses.parse).acreate(*args, **kwargs)
+        response = await ResponseWrapper(None, self.__responses.parse).acreate(*args, **kwargs)
         return AsyncResponseWrapper(response)
 
 
