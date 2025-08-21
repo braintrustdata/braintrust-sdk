bcf2b4cb01eee347555b90c5ced7b432eacf01c0
Olmo Maldonado <olmo@braintrust.dev>
2025-08-20 19:18:13 -0500

    remove extra warnings and fix import test

--- integrations/adk-py/tests/conftest.py
+++ integrations/adk-py/tests/conftest.py
@@ -9,6 +9,7 @@ def setup():
     os.environ.setdefault("BRAINTRUST_API_KEY", "test")
     os.environ["BRAINTRUST_APP_URL"] = "https://www.braintrust.dev/"
     os.environ["BRAINTRUST_API_URL"] = "https://api.braintrust.dev/"
+    os.environ.setdefault("GOOGLE_API_KEY", "test")
     force_tracer_provider()
     init_logger(project="braintrust-adk")
     yield
