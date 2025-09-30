936f4726126e013d6c1a0f19128681e66ee607c3
Olmo Maldonado <olmo@braintrust.dev>
2025-09-30 13:21:32 -0700

    include a root span for display/test purposes

--- py/examples/genai/quickstart.py
+++ py/examples/genai/quickstart.py
@@ -1,3 +1,4 @@
+from braintrust.logger import start_span
 from braintrust.wrappers.genai import setup_genai
 from google import genai
 from google.genai import types
@@ -6,11 +7,12 @@ setup_genai(project_name="example-genai-py")
 
 client = genai.Client()
 
-for chunk in client.models.generate_content_stream(
-    model="gemini-2.5-flash",
-    contents="Explain how AI works in a few words",
-    config=types.GenerateContentConfig(
-        thinking_config=types.ThinkingConfig(thinking_budget=0)  # Disables thinking,
-    ),
-):
-    print(chunk.text)
+with start_span(name="root span"):
+    for chunk in client.models.generate_content_stream(
+        model="gemini-2.5-flash",
+        contents="Explain how AI works in a few words",
+        config=types.GenerateContentConfig(
+            thinking_config=types.ThinkingConfig(thinking_budget=0)  # Disables thinking,
+        ),
+    ):
+        print(chunk.text)
