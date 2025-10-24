2d8afda70e0f82176d7ee80bcb653ed09d4ada76
Alex Steere <dasteere@live.com>
2025-10-24 13:52:55 -0700

    SSF ast changes (#1014)
    Need to sync ast changes

--- js/btql/ast.ts
+++ js/btql/ast.ts
@@ -256,6 +256,18 @@ export const btqlSnippetSchema = z.strictObject({
 });
 export type BtqlSnippet = z.infer<typeof btqlSnippetSchema>;
 
+export type SingleSpanFilter = {
+  op: "singlespanfilter";
+  expr: Expr;
+  loc?: NullableLoc;
+};
+export const singleSpanFilterSchema: z.ZodType<SingleSpanFilter> =
+  z.strictObject({
+    op: z.literal("singlespanfilter"),
+    expr: z.lazy(() => exprSchema),
+    loc,
+  });
+
 export type Expr =
   | Literal
   | Interval
@@ -269,7 +281,8 @@ export type Expr =
   | UnaryArithmeticExpr
   | UnaryExpr
   | ArithmeticExpr
-  | BtqlSnippet;
+  | BtqlSnippet
+  | SingleSpanFilter;
 
 export const exprSchema: z.ZodType<Expr> = z.union([
   literalSchema,
@@ -285,6 +298,7 @@ export const exprSchema: z.ZodType<Expr> = z.union([
   unaryExprSchema,
   arithmeticExprSchema,
   btqlSnippetSchema,
+  singleSpanFilterSchema,
 ]);
 
 export const aliasExpr = z.strictObject({
