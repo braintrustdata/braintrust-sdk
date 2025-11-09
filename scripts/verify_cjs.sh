ba5cb2309ce6ecaebdb3d11fd0a870769d187c0d
Caitlin Pinn <caitlin.pinn@gmail.com>
2025-11-08 16:01:31 -0800

    downgrade vitest

--- scripts/verify_cjs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-node -e '
-if (typeof require === "undefined") {
-  console.error("Error: Not running in CommonJS");
-  process.exit(1);
-} else {
-  console.log("Running in CommonJS");
-}
-'
--- scripts/verify_esm.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-node -e '
-if (typeof import.meta.url !== "string") {
-  console.error("Error: Not running in ESM");
-  process.exit(1);
-} else {
-  console.log("Running in ESM");
-}
-'
