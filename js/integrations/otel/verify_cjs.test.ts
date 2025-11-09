c58564a6f9a2933905b12a2c015969a17cbd0781
Caitlin Pinn <caitlin.pinn@gmail.com>
2025-11-08 19:40:18 -0800

    give up on vitest as it is really ESM only, run as regular examples again

--- js/integrations/otel/verify_cjs.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "vitest";
-
-test("CommonJS environment exposes require", () => {
-  const nodeRequire = globalThis.require;
-  expect(typeof nodeRequire).toBe("function");
-});
-
