4cc382b252ad771b3d46b1ea6f056c942e5d12e1
Caitlin Pinn <caitlin.pinn@gmail.com>
2025-11-13 16:09:49 -0800

    attempt to fix nunjucks in the build

--- js/src/template/nunjucks-render.test.ts
+++ js/src/template/nunjucks-render.test.ts
@@ -1,18 +1,19 @@
 import { describe, test, expect } from "vitest";
 import { lintTemplate } from "./nunjucks-utils";
-import { nunjucks } from "./nunjucks-browser";
+import { nunjucks } from "./nunjucks";
 
 function getEnv() {
-  const N = nunjucks.default || nunjucks;
-  return new N.Environment(null, {
+  return new nunjucks.Environment(null, {
     autoescape: false,
     throwOnUndefined: false,
   });
 }
 
 function getStrictEnv() {
-  const N = nunjucks.default || nunjucks;
-  return new N.Environment(null, { autoescape: false, throwOnUndefined: true });
+  return new nunjucks.Environment(null, {
+    autoescape: false,
+    throwOnUndefined: true,
+  });
 }
 
 describe("nunjucks rendering", () => {
