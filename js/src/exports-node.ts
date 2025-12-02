e00a411dae3f20f0ccec8dca08b9858c4f0c4a95
Caitlin Pinn <caitlin.pinn@gmail.com>
2025-12-01 20:13:28 -0800

    add comments about the node differences

--- js/src/exports-node.ts
+++ js/src/exports-node.ts
@@ -1,5 +1,7 @@
 // Node exports: common exports + node-only exports
 export * from "./exports-common";
+
+// framework includes a dependency on process.stdout.write which is node only
 export {
   BaseExperiment,
   Evaluator,
@@ -20,5 +22,7 @@ export {
   runEvaluator,
   defaultErrorScoreHandler,
 } from "./framework";
+// framework2 includes a dependency __filename which is node only
 export * from "./framework2";
+// wrapAISDKModel is being deprecated and was never exported for browser builds.
 export { wrapAISDKModel } from "./wrappers/ai-sdk";
