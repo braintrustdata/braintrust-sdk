1ad84b9218e4df6cf188a27ba0eb6e50402d0bb0
Caitlin Pinn <caitlin.pinn@gmail.com>
2025-12-01 19:41:08 -0800

    re-separate backend only types

--- js/src/exports-types.ts
+++ js/src/exports-types.ts
@@ -1,5 +1,5 @@
-// Type-only exports both for browser and node builds
-// Necessary for TypeScript type checking in both browser and node builds
+// Browser-compatible type exports only
+// Node only types are in exports-node.ts
 export type {
   ParentExperimentIds,
   ParentProjectLogIds,
@@ -13,17 +13,4 @@ export type {
   CommentEvent,
   DatasetRecord,
 } from "../util/index";
-
-// Types below are for node builds only
-export type {
-  EvaluatorManifest,
-  EvalParameterSerializedSchema,
-  EvaluatorDefinition,
-  EvaluatorDefinitions,
-} from "../dev/types";
-export {
-  evaluatorDefinitionSchema,
-  evaluatorDefinitionsSchema,
-} from "../dev/types";
-export type { EvalParameters } from "./eval-parameters";
 export type { AttachmentReference } from "./generated_types";
