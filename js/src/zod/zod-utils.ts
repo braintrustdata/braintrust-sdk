98a69f1ffe010bdb105aa6bbedbbbc0e0f626727
Caitlin Pinn <caitlin.pinn@gmail.com>
2025-12-29 22:34:40 -0800

    remove the zod v3 output

--- js/src/zod/zod-utils.ts
+++ js/src/zod/zod-utils.ts
@@ -1,24 +1,13 @@
-import * as z3 from "zod/v3";
-import * as z4 from "zod/v4";
 import { zodToJsonSchema as zodToJsonSchemaV3 } from "zod-to-json-schema";
 
-function isZodV4(zodObject: z3.ZodType | z4.ZodType): zodObject is z4.ZodType {
-  return (
-    typeof zodObject === "object" &&
-    zodObject !== null &&
-    "_zod" in zodObject &&
-    (zodObject as any)._zod !== undefined
-  );
-}
-
-export function zodToJsonSchema(schema: z3.ZodType | z4.ZodType) {
-  if (isZodV4(schema)) {
-    return z4.toJSONSchema(schema as z4.ZodType, {
+export function zodToJsonSchema(schema: any) {
+  if (schema && typeof (schema as any).toJSONSchema === "function") {
+    return (schema as any).toJSONSchema(schema as any, {
       target: "draft-7",
     });
   }
   // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-  return zodToJsonSchemaV3(schema as z3.ZodType);
+  return zodToJsonSchemaV3(schema as any);
 }
 
 export function getDescription(schema: unknown): string | undefined {
