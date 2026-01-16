131457322c311da83dd161889c4bbb9647bd2086
Alex Z <alex.zelenskiy@braintrustdata.com>
2026-01-15 19:50:47 -0800

    wip otel stuff

--- py/src/braintrust/logger.py
+++ py/src/braintrust/logger.py
@@ -396,6 +396,7 @@ class BraintrustState:
         from braintrust.span_cache import SpanCache
 
         self.span_cache = SpanCache()
+        self._otel_flush_callback: Any | None = None
 
     def reset_login_info(self):
         self.app_url: str | None = None
@@ -452,6 +453,21 @@ class BraintrustState:
                     self._context_manager = get_context_manager()
 
         return self._context_manager
+
+    def register_otel_flush(self, callback: Any) -> None:
+        """
+        Register an OTEL flush callback. This is called by the OTEL integration
+        when it initializes a span processor/exporter.
+        """
+        self._otel_flush_callback = callback
+
+    async def flush_otel(self) -> None:
+        """
+        Flush OTEL spans if a callback is registered.
+        Called during ensure_spans_flushed to ensure OTEL spans are visible in BTQL.
+        """
+        if self._otel_flush_callback:
+            await self._otel_flush_callback()
 
     def copy_state(self, other: "BraintrustState"):
         """Copy login information from another BraintrustState instance."""
@@ -1760,6 +1776,25 @@ def login(
     # Only permit one thread to login at a time
     with login_lock:
         _state.login(app_url=app_url, api_key=api_key, org_name=org_name, force_login=force_login)
+
+
+def register_otel_flush(callback: Any) -> None:
+    """
+    Register a callback to flush OTEL spans. This is called by the OTEL integration
+    when it initializes a span processor/exporter.
+
+    When ensure_spans_flushed is called (e.g., before a BTQL query in scorers),
+    this callback will be invoked to ensure OTEL spans are flushed to the server.
+
+    Also disables the span cache, since OTEL spans aren't in the local cache
+    and we need BTQL to see the complete span tree (both native + OTEL spans).
+
+    :param callback: The async callback function to flush OTEL spans.
+    """
+    global _state
+    _state.register_otel_flush(callback)
+    # Disable span cache since OTEL spans aren't in the local cache
+    _state.span_cache.disable()
 
 
 def login_to_state(
