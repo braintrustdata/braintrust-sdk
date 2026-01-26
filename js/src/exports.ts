c935b1e5dee6cda9e9a0d1ca408af4454844ad08
Joshua Wootonn <joshuawootonn@gmail.com>
2026-01-26 12:25:01 -0600

    Update exports to include ParameterSource

--- js/src/exports.ts
+++ js/src/exports.ts
@@ -226,6 +226,7 @@ export type {
   EvalParameterSerializedSchema,
   EvaluatorDefinition,
   EvaluatorDefinitions,
+  ParametersSource,
 } from "../dev/types";
 
 export type { EvalParameters } from "./eval-parameters";
