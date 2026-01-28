e29564102c0cae2048d0d3ceab95b9a9ce84a6f3
Aswin Karumbunathan <aswin@braintrustdata.com>
2026-01-28 11:37:33 -0800

    BRA-4024 Remove duplicate retry in Python SDK HTTPConnection.get_json
    There is a generic retry handler that should make this unnecessary

--- py/src/braintrust/logger.py
+++ py/src/braintrust/logger.py
@@ -762,20 +762,10 @@ class HTTPConnection:
     def delete(self, path: str, *args: Any, **kwargs: Any) -> requests.Response:
         return self.session.delete(_urljoin(self.base_url, path), *args, **kwargs)
 
-    def get_json(self, object_type: str, args: Mapping[str, Any] | None = None, retries: int = 0) -> Mapping[str, Any]:
-        # FIXME[matt]: the retry logic seems to be unused and could be n*2 because of the the retry logic
-        # in the RetryRequestExceptionsAdapter. We should probably remove this.
-        tries = retries + 1
-        for i in range(tries):
-            resp = self.get(f"/{object_type}", params=args)
-            if i < tries - 1 and not resp.ok:
-                _logger.warning(f"Retrying API request {object_type} {args} {resp.status_code} {resp.text}")
-                continue
-            response_raise_for_status(resp)
-
-            return resp.json()
-        # Needed for type checking.
-        raise Exception("unreachable")
+    def get_json(self, object_type: str, args: Mapping[str, Any] | None = None) -> Mapping[str, Any]:
+        resp = self.get(f"/{object_type}", params=args)
+        response_raise_for_status(resp)
+        return resp.json()
 
     def post_json(self, object_type: str, args: Mapping[str, Any] | None = None) -> Any:
         resp = self.post(f"/{object_type.lstrip('/')}", json=args)
@@ -3815,7 +3805,6 @@ class Experiment(ObjectFetcher[ExperimentEvent], Exportable):
                         "experiment_id": self.id,
                         "base_experiment_id": comparison_experiment_id,
                     },
-                    retries=3,
                 )
             except Exception as e:
                 _logger.warning(
@@ -4627,7 +4616,6 @@ class Dataset(ObjectFetcher[DatasetEvent]):
                 args={
                     "dataset_id": self.id,
                 },
-                retries=3,
             )
             data_summary = DataSummary(new_records=self.new_records, **data_summary_d)
 
