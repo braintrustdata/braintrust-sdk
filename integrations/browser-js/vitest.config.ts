d2cf41a476ea8653ab3835947d0943955631e93b
Luca Forstner <luca.forstner@gmail.com>
2026-03-02 17:15:27 -0800

    Rm script instead of using passWithNoTests

--- integrations/browser-js/vitest.config.ts
+++ integrations/browser-js/vitest.config.ts
@@ -3,6 +3,5 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["tests/**/*.test.ts", "src/**/*.test.ts"],
-    passWithNoTests: true,
   },
 });
