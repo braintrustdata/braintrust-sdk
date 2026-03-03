ca9bc05edde31d81a0999ea8a0fe1dc67250124b
Luca Forstner <luca.forstner@gmail.com>
2026-03-02 16:51:52 -0800

    test: Facilitate test onboarding by picking up `.env` file

--- integrations/browser-js/vitest.config.ts
+++ integrations/browser-js/vitest.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["tests/**/*.test.ts", "src/**/*.test.ts"],
+    passWithNoTests: true,
   },
 });
