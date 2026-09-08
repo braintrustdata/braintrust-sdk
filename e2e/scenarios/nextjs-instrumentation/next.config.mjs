7ab7a1ddd8b005d39154099ec066965885f740d4
lforst <8118419+lforst@users.noreply.github.com>
2026-09-08 16:16:35 +0000

    fix: Inject instrumentation hook on Next.js versions <15

--- e2e/scenarios/nextjs-instrumentation/next.config.mjs
+++ e2e/scenarios/nextjs-instrumentation/next.config.mjs
@@ -1,14 +1,13 @@
+import { wrapNextjsConfigWithBraintrust } from "braintrust/next";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  experimental: {
-    instrumentationHook: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
 };
 
-export default nextConfig;
+export default wrapNextjsConfigWithBraintrust(nextConfig);
